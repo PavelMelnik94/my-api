17d42c07b8aef0a696efcc415df769376d06ee0c
pavelMelnik <p.melnik@intelico.su>
2023-02-26 16:02:11 +0300

    add dto schemas in swagger

--- src/user/dto/login-user.dto.ts
+++ src/user/dto/login-user.dto.ts
@@ -1,5 +1,16 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
+import { ApiProperty } from "@nestjs/swagger";
+import { IsEmail, IsNotEmpty } from "class-validator";
+import { User } from ".prisma/client";
 
 export class LoginUserDto extends PartialType(CreateUserDto) {
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsEmail()
+  readonly email?: User['email']
+
+  @ApiProperty()
+  @IsNotEmpty()
+  readonly password?: User['password'] ;
 }
--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,4 +1,19 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
+import { ApiProperty } from "@nestjs/swagger";
+import { User } from ".prisma/client";
+import { IsEmail, IsNotEmpty } from "class-validator";
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @ApiProperty()
+  readonly userName?: User['userName']
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsEmail()
+  readonly email?: User['email']
+
+  @ApiProperty()
+  @IsNotEmpty()
+  readonly password?: User['password'] ;
+}
